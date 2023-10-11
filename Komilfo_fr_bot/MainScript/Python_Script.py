d4745a3aee0f5323e1d2271b646746d4c454c1e2
Sflegioner <117868151+Sflegioner@users.noreply.github.com>
2023-10-11 22:30:21 +0200

    Reminder_version_01

--- Komilfo_fr_bot/MainScript/Python_Script.py
+++ Komilfo_fr_bot/MainScript/Python_Script.py
@@ -1,16 +1,23 @@
 API_Token = '6470853060:AAEZidQy_4m2qWDwQ7ZtUCF7nxZQ7TvX8d8' #token
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters  
 import Words as W
+import time
 
 
 async def start(update, context):
     user = update.effective_user
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Привітик це бот для вивчення французької мови. Бот знаходиться в розробці, якщо потрібно більше інформації напиши /help")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, if you need more information: /help")
 async def help(update,context):
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Ось список команд:\n1)/start\n2)/help\n3)/random")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="List of commands:\n1)/start\n2)/help\n3)/random\n4)/reminder")
 async def random(update,context):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=W.listW)
+async def reminder(update,context):
+    await context.bot.send_message(chat_id=update.effective_chat.id,text = "send time in minuts")
+    user = update.message.from_user
+    message = update.message.text
+    await context.bot.send_message(chat_id=update.effective_chat.id,text = user)
+    
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(API_Token).build()
@@ -18,10 +25,12 @@ if __name__ == '__main__':
     start_handler = CommandHandler('start',start)
     help_handler = CommandHandler('help',help)
     random_handler = CommandHandler('random',random)
-
+    reminder_handler = CommandHandler('reminder',reminder)
+    
 
     application.add_handler(start_handler)
     application.add_handler(help_handler)
     application.add_handler(random_handler)
+    application.add_handler(reminder_handler)
 
     application.run_polling()
